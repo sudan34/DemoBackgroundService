6fd99260b9cb6d7b7d5aa83faebd27f387483bd7
sudan <madhusudan.bhattarai@nepalpayment.com>
2025-01-19 12:58:26 +0545

    save log of info and error

--- DemoBackgroundService/Program.cs
+++ DemoBackgroundService/Program.cs
@@ -5,13 +5,23 @@ using DemoBackgroundService.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Serilog
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console() // Log to the console
-    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Log to a file with daily rolling
-    .CreateLogger();
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo.Console() // Log to the console
+//    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Log to a file with daily rolling
+//    .CreateLogger();
 
 // Use Serilog as the logging provider
-builder.Host.UseSerilog();
+builder.Host.UseSerilog((hostconfig, config) =>
+{
+    config.ReadFrom.Configuration(hostconfig.Configuration);
+    
+    // Log only Information and Error levels
+    config.MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning); // Avoid excessive framework logs
+    config.MinimumLevel.Information(); // Global minimum level is Information
+
+    config.WriteTo.Console();
+    config.WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day);
+});
 
 //Add services to the container
 builder.Services.AddSingleton<ITaskQueue, TaskQueue>();
